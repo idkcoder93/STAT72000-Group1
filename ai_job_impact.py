131700261ec9bcbc9479b364a4b454951897e1a6
idkcoder93 <afridman0793@conestogac.on.ca>
2024-04-16 20:08:58 -0400

    some graphs are plotted some graphs do not make sense

--- ai_job_impact.py
+++ ai_job_impact.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sb
+from scipy.stats import bernoulli
 
 # Read CSV file into DataFrame df
 df = pd.read_csv('ai_job_data.csv', index_col=0)
@@ -16,7 +18,7 @@ std = df["AI Impact"].std()
 print(std)
 
 # Plotting the graph of job titles and AI impact relativeness
-first_30_job_titles = df.index[:30]  # Assuming the job titles are in the index
+first_30_job_titles = df.index[:30] 
 first_30_AI_workplace_impact_ratio = df["AI_Workload_Ratio"].head(30)
 
 # Create a bar plot
@@ -33,4 +35,46 @@ plt.title('AI Impact on Jobs')
 # Show the plot
 plt.show()
 
-# Probability of AI impact
+# Probability of AI impact on the all unique domains
+AI_rate = average_ai_impact
+unique_domain_count = df["Domain"].nunique() # counts the different types of domains
+
+# Bernoulli distrubtion showing the AI rate of all the domains in the dataset
+data = bernoulli.rvs(size = unique_domain_count , p = AI_rate)
+
+#Visualizing the results
+sb.set_style('whitegrid')
+sb.displot(data, discrete=True, shrink=.8 , color = 'k')
+plt.title('Probability of AI Impact on Domain')
+plt.ylabel('Number of domains')
+plt.xlabel('Probability of domain')
+plt.show()
+
+#I believe the data appears to be uniformally distributed
+data = df["AI Impact"].head(30)
+
+# Plot the density plot for your dataset
+pd.DataFrame(data).plot(kind='density')
+
+# Customize the plot (optional)
+plt.xlabel('AI Impact')
+plt.ylabel('Density')
+plt.title('Density Plot of AI Impact')
+
+# Show the plot
+plt.show()
+
+# graph that shows the domain and their AI score
+grouped_data = df.groupby("Domain")["AI Impact"].mean().head(30)
+
+# Extracting data
+first_30_domain = grouped_data.index
+first_30_AI = grouped_data.values
+
+# Plotting
+plt.plot(first_30_domain, first_30_AI)
+plt.xlabel('Domain')
+plt.xticks(rotation=90)
+plt.ylabel('Average AI Score')
+
+plt.show()
